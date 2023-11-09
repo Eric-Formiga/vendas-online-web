99781f3eaecad75d15331ddd0f7e2973b68c06bb
Eric-Formiga <eric.13formiga@gmail.com>
2023-11-09 15:04:47 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from "../styles/loginSceen.styles"
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackgroundImage src="./background.png"/>
+    </div>
+  )
+}
+
+export default LoginScreen
